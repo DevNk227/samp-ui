4ce89f236c785a226a0c9aa14c1a2fc446de7058
Teccom_Studio <nkzaza78@gmail.com>
2026-03-10 12:43:28 +0700

    Fix ES Modules Export

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}", // <--- เพิ่มบรรทัดนี้เผื่อคุณใช้โฟลเดอร์ src
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
